4f1bde07aca0c98b39bc13c877164df11f36c564
Hassan Khan <144674805+HassanKhan5588@users.noreply.github.com>
2024-01-28 19:52:32 +0500

    added files

---
+++ app.js
@@ -0,0 +1,118 @@
+// 1. pop
+// let Fruits = ["Apple", "Banana", "Orange", "Mango", "Cheery"];
+// Fruits.pop();
+// console.log(Fruits);
+
+
+//  2. push 
+// var numbers = [1, 2, 3, ];
+// numbers.push(4);
+// console.log(numbers);
+
+// 3. shift
+//  let colors = ['red', 'blue', 'green','black','white','peach'];
+//     colors.shift();
+//     console.log(colors);
+
+// 4. unshift 
+// let programmingLanguages = ["JavaScript", "Python", "Java", "C++"];
+// programmingLanguages.unshift("HTML");
+// console.log(programmingLanguages);
+
+// 5. slice
+// let daysOfWeek = ['Saturday','Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday' ];
+// let weekdays = daysOfWeek.slice(2, 7);
+// console.log(weekdays);
+
+// 6. reverse
+//  let reverseArry = ['H', 'a', 's', 's', 'a', 'n'];
+// reverseArry.reverse();
+// console.log(reverseArry.join(''));
+
+
+// 7. concat
+// let fruits = ['apple', 'banana', 'orange'];
+// let vegetables = ['carrot', 'ladyfinger', 'spinach'];
+// let mergeArray = fruits.concat(vegetables);
+// console.log(mergeArray);
+
+// 8. join
+//  let sentenceArray = ['My', 'Name', 'is', 'Hassan'];
+// let sentence = sentenceArray.join(' ');
+// console.log(sentence);
+
+// 9. splice
+//  let numbers = [1, 2, 3, 4, 5, 6];
+// numbers.splice(1, 3);
+// console.log(numbers);
+
+
+// 10. map
+// let numbers = [1, 2, 3, 4, 5];
+// let doubledNumbers = numbers.map(num => num *2);
+// console.log(doubledNumbers);
+
+// 11. filter
+// let age = [18, 24, 25, 36, 41, 55];
+// let above21 = age.filter(age => age > 21);
+// console.log(above21);
+
+// 12. every 
+// const marks = [66, 82, 95, 68, 89];
+// const YourMarksGreaterThan60 = marks.every(score => score > 60);
+// if (YourMarksGreaterThan60) {
+//     console.log(' Marks are greater than 60.');
+// } else {
+//     console.log('Not Marks are greater than 60.');
+// }
+
+// 13. forEach 
+// const names = ['Hassan', 'Hunain', 'Saad', 'Ali'];
+// names.forEach(name => {
+//     console.log(`Hello, ${name}`);
+// });
+
+// 14. includes
+// const cities = ['Karachi', 'Islamabad', 'Punjab', 'Lahore'];
+// const cityToCheck = 'Karachi';
+// const isInArray = cities.includes(cityToCheck);
+// if (isInArray) {
+//     console.log(`${cityToCheck} is in the array.`);
+// } else {
+//     console.log(`${cityToCheck} is not in the array.`);
+// }
+
+// 15. reduce
+// const numbers = [1, 2, 3, 4, 5];
+// const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+// console.log(sum);
+
+// 16. indexof
+// const colors = ['red', 'blue', 'green', 'black', 'orange'];
+// const colorToFind = 'black';
+// const indexOfColor = colors.indexOf(colorToFind);
+// console.log(`${colorToFind} ${indexOfColor}`);
+
+// 17. lastindexof
+// const myArray = [1, 2, 3, 4, 7, 5, 1, 6, 7, 2, 8];
+// const targetValue = 4;
+// const lastIndex = myArray.lastIndexOf(targetValue);
+// console.log(`${targetValue}${lastIndex}`);
+
+// 18. toString
+// const mix = [10, "Hassan", true, 10.11, false];
+// const arrayToString = mix.toString();
+// console.log(arrayToString);
+
+// 19. some
+// var temperature = [24, 28, 23, 34, 22];
+// var Above30 = temperature.some(temperature => temperature > 30);
+// if (Above30) {
+//   console.log(' 30 degrees sy above hai.');
+// } else {
+//   console.log('30 degrees sy above nhi hai temperatures.');
+// }
+
+
+
+
